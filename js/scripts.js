8cfdb0a44d51ac200ab8ce0ad0dac141191b4836
SoniaRajs <sonia.ryszytylo@gmail.com>
2017-09-18 01:00:42 +0200

    changes

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+var a, h;
+
+function getTriangleArea (a, h) {
+	var wartoscDzialania;
+
+	if  ( (a > 0) && (h > 0) ) {
+			wartoscDzialania = a*h/2;
+	} else {
+			console.log('Nieprawidłowe dane');
+	}
+
+	return wartoscDzialania;
+}
+
+console.log( getTriangleArea(10, 6) )
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 1);
+var triangle3Area = getTriangleArea(-4, 0);
+
+console.log(triangle1Area, triangle2Area, triangle3Area);
